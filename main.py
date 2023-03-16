bcbf2148666686e5da648bc524619b30fbc340af
Anes <suhonjicanes96@gmail.com>
2023-03-16 18:57:28 -0500

    Add links, specify language, limit amount of news, add dynamic topic control

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
 from send_email import send_email
 
+topic = "techcrunch"
 
 api_key = "58bc1a1409de4ec3824701545189e709"
-url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=" \
-						"58bc1a1409de4ec3824701545189e709"
+url = "https://newsapi.org/v2/top-headlines?" \
+						f"sources={topic}&" \
+						"apiKey=58bc1a1409de4ec3824701545189e709&language=en"
 
 # Make request
 r = requests.get(url)
@@ -14,11 +16,13 @@ content = r.json()
 
 body = ""
 # Access article titles and descriptions
-for article in content["articles"]:
-			body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+for article in content["articles"][:20]:
+			body = body + article["title"] + "\n"\
+										+ article["description"] + "\n"\
+										+ article["url"] +  2*"\n"
 
 message = f"""\
-Subject: News
+Subject: Today's News
 
 {body} 
 """
